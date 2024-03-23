cb7e553b67e2bf56f126c9fdb73a05ddff52e934
Nate LaClaire <nate@natelaclaire.com>
2024-03-23 13:21:54 -0400

    Through tutorial 7

---
+++ core/src/com/natelaclaire/mariobros/MarioBros.java
@@ -0,0 +1,30 @@
+package com.natelaclaire.mariobros;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.natelaclaire.mariobros.Screens.PlayScreen;
+
+public class MarioBros extends Game {
+	public SpriteBatch batch;
+	public static final int V_WIDTH = 400;
+	public static final int V_HEIGHT = 208;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+		super.dispose();
+	}
+}
